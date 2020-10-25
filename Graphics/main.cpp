ca6604051b080896bc00cb56d16696d9941b859f
Oscar C <oscar.coghlan@gmail.com>
2020-10-25 15:46:29 +1100

    now adds values to z axis

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -4,22 +4,28 @@
 #include <classes.h>
 
 int main() {
-    std::cout << "ur mom";
-    bool world[100][100][100] = {{false}};
+    bool world[100][100][100] = {{false}}; //world[z][x][y]
     bool plane[100][100] = {{false}};
     printToConsole(plane);
     for(int z = 0; z < 100; z++) {
         for(int x = 0; x < 100; x++) {
-            plane[x][Hemisphere(x,z,50,50,50,40,1)] = true;
-            plane[x][Hemisphere(x,z,50,50,50,40,-1)] = true;
+            plane[x][Hemisphere(x,z,50,50,50,30,1)] = true;
+            plane[x][Hemisphere(x,z,50,50,50,30,-1)] = true;
         }
-        printToConsole(plane);
-        _sleep(5);
-        system("cls");
+        for(size_t y = 0; y < 100; y++) {
+            for(size_t x = 0; x < 100; x++) {
+                world[z][x][y] = plane[x][y];
+            }
+        }
         for(size_t y = 0; y < 100; y++) {
             for(size_t x = 0; x < 100; x++) {
                 plane[x][y] = false;
             }
         }
+    }
+    for(size_t z = 0; z < 100; z++) {
+        printToConsole(world[z]);
+        _sleep(5);
+        system("cls");
     }
 }
